1a76c1399df084b84983618dc2e91a62dd93811c
lexmarcos <marcosuel.vieira@hotmail.com>
2026-05-26 23:22:36 -0300

    feat: export newly added components

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -6,8 +6,18 @@ export { default as AlertDialogDescription } from './components/AlertDialogDescr
 export { default as AlertDialogFooter } from './components/AlertDialogFooter.svelte';
 export { default as AlertDialogHeader } from './components/AlertDialogHeader.svelte';
 export { default as AlertDialogTitle } from './components/AlertDialogTitle.svelte';
+export { default as Accordion } from './components/Accordion.svelte';
+export { default as AccordionContent } from './components/AccordionContent.svelte';
+export { default as AccordionItem } from './components/AccordionItem.svelte';
+export { default as AccordionTrigger } from './components/AccordionTrigger.svelte';
 export { default as Avatar } from './components/Avatar.svelte';
 export { default as Badge } from './components/Badge.svelte';
+export { default as Breadcrumb } from './components/Breadcrumb.svelte';
+export { default as BreadcrumbEllipsis } from './components/BreadcrumbEllipsis.svelte';
+export { default as BreadcrumbItem } from './components/BreadcrumbItem.svelte';
+export { default as BreadcrumbLink } from './components/BreadcrumbLink.svelte';
+export { default as BreadcrumbList } from './components/BreadcrumbList.svelte';
+export { default as BreadcrumbSeparator } from './components/BreadcrumbSeparator.svelte';
 export { default as Button } from './components/Button.svelte';
 export { default as Card } from './components/Card.svelte';
 export { default as CardBody } from './components/CardBody.svelte';
@@ -15,11 +25,48 @@ export { default as CardFooter } from './components/CardFooter.svelte';
 export { default as CardHeader } from './components/CardHeader.svelte';
 export { default as Checkbox } from './components/Checkbox.svelte';
 export { default as Collapsible } from './components/Collapsible.svelte';
+export { default as Combobox } from './components/Combobox.svelte';
+export { default as ContextMenu } from './components/ContextMenu.svelte';
+export { default as ContextMenuCheckboxItem } from './components/ContextMenuCheckboxItem.svelte';
+export { default as ContextMenuContent } from './components/ContextMenuContent.svelte';
+export { default as ContextMenuGroup } from './components/ContextMenuGroup.svelte';
+export { default as ContextMenuItem } from './components/ContextMenuItem.svelte';
+export { default as ContextMenuLabel } from './components/ContextMenuLabel.svelte';
+export { default as ContextMenuPortal } from './components/ContextMenuPortal.svelte';
+export { default as ContextMenuRadioGroup } from './components/ContextMenuRadioGroup.svelte';
+export { default as ContextMenuRadioItem } from './components/ContextMenuRadioItem.svelte';
+export { default as ContextMenuSeparator } from './components/ContextMenuSeparator.svelte';
+export { default as ContextMenuShortcut } from './components/ContextMenuShortcut.svelte';
+export { default as ContextMenuSub } from './components/ContextMenuSub.svelte';
+export { default as ContextMenuSubContent } from './components/ContextMenuSubContent.svelte';
+export { default as ContextMenuSubTrigger } from './components/ContextMenuSubTrigger.svelte';
+export { default as ContextMenuTrigger } from './components/ContextMenuTrigger.svelte';
 export { default as CollapsiblePanel } from './components/CollapsiblePanel.svelte';
 export { default as CollapsibleTrigger } from './components/CollapsibleTrigger.svelte';
+export { default as DataTable } from './components/DataTable.svelte';
+export { default as DatePicker } from './components/DatePicker.svelte';
+export { default as Dialog } from './components/Dialog.svelte';
+export { default as DialogClose } from './components/DialogClose.svelte';
+export { default as DialogContent } from './components/DialogContent.svelte';
+export { default as DialogDescription } from './components/DialogDescription.svelte';
+export { default as DialogFooter } from './components/DialogFooter.svelte';
+export { default as DialogHeader } from './components/DialogHeader.svelte';
+export { default as DialogOverlay } from './components/DialogOverlay.svelte';
+export { default as DialogPortal } from './components/DialogPortal.svelte';
+export { default as DialogTitle } from './components/DialogTitle.svelte';
+export { default as DialogTrigger } from './components/DialogTrigger.svelte';
+export { default as Drawer } from './components/Drawer.svelte';
+export { default as DrawerClose } from './components/DrawerClose.svelte';
+export { default as DrawerContent } from './components/DrawerContent.svelte';
+export { default as DrawerFooter } from './components/DrawerFooter.svelte';
+export { default as DrawerHeader } from './components/DrawerHeader.svelte';
 export { default as Dropdown } from './components/Dropdown.svelte';
 export { default as DropdownItem } from './components/DropdownItem.svelte';
 export { default as DropdownSeparator } from './components/DropdownSeparator.svelte';
+export { default as Empty } from './components/Empty.svelte';
+export { default as Popover } from './components/Popover.svelte';
+export { default as PopoverAnchor } from './components/PopoverAnchor.svelte';
+export { default as PopoverClose } from './components/PopoverClose.svelte';
 export { default as Progress } from './components/Progress.svelte';
 export { default as RadioGroup } from './components/RadioGroup.svelte';
 export { default as RadioItem } from './components/RadioItem.svelte';
@@ -39,16 +86,47 @@ export { default as TabList } from './components/TabList.svelte';
 export { default as TabPanel } from './components/TabPanel.svelte';
 export { default as TabTrigger } from './components/TabTrigger.svelte';
 export { default as TextInput } from './components/TextInput.svelte';
+export { default as Textarea } from './components/Textarea.svelte';
 export { default as Tooltip } from './components/Tooltip.svelte';
 export type { AlertProps, AlertVariant } from './components/alert';
+export type { AccordionContentProps, AccordionHeadingLevel, AccordionItemProps, AccordionProps, AccordionSize, AccordionTriggerProps, AccordionType, AccordionVariant } from './components/accordion';
 export type { AvatarProps, AvatarSize } from './components/avatar';
 export type { BadgeProps, BadgeSize, BadgeStyle, BadgeVariant } from './components/badge';
+export type { BreadcrumbEllipsisProps, BreadcrumbItemProps, BreadcrumbLinkProps, BreadcrumbListProps, BreadcrumbProps, BreadcrumbSeparatorProps } from './components/breadcrumb';
 export type { ButtonProps, ButtonSize, ButtonVariant } from './components/button';
 export type { CardBodyProps, CardFooterProps, CardHeaderProps, CardProps, CardSize, CardVariant } from './components/card';
 export type { AlertDialogActionProps, AlertDialogCancelProps, AlertDialogContentProps, AlertDialogDescriptionProps, AlertDialogFooterProps, AlertDialogHeaderProps, AlertDialogTitleProps } from './components/dialog';
 export type { CheckboxProps } from './components/checkbox';
 export type { CollapsiblePanelProps, CollapsibleProps, CollapsibleTriggerProps, CollapsibleVariant } from './components/collapsible';
+export type { ComboboxProps, ComboboxSize } from './components/combobox';
+export type { ContextMenuCheckboxItemProps, ContextMenuContentProps, ContextMenuGroupProps, ContextMenuItemProps, ContextMenuLabelProps, ContextMenuRadioGroupProps, ContextMenuRadioItemProps, ContextMenuSeparatorProps, ContextMenuShortcutProps, ContextMenuSubContentProps, ContextMenuSubTriggerProps, ContextMenuTriggerProps } from './components/context-menu';
+export type {
+  DataTableProps,
+  DataTableColumn,
+  DataTableDensity,
+  DataTableSize,
+  DataTableColumnAlign,
+  CellContext as DataTableCellContext,
+  HeaderContext as DataTableHeaderContext
+} from './components/data-table';
+export type { DatePickerChangeDetails, DatePickerDateValue, DatePickerFormat, DatePickerMode, DatePickerProps, DatePickerRange, DatePickerRangeValue, DatePickerSize, DatePickerState, DatePickerValue, DatePickerVariant } from './components/date-picker';
+export type {
+  DialogCloseProps,
+  DialogContentProps,
+  DialogDescriptionProps,
+  DialogFooterProps,
+  DialogHeaderProps,
+  DialogOverlayProps,
+  DialogPortalProps,
+  DialogProps,
+  DialogSize,
+  DialogTitleProps,
+  DialogTriggerProps
+} from './components/dialog-base';
+export type { DrawerCloseProps, DrawerContentProps, DrawerFooterProps, DrawerHeaderProps, DrawerPosition, DrawerProps, DrawerTriggerProps } from './components/drawer';
 export type { DropdownItemProps, DropdownTriggerProps } from './components/dropdown';
+export type { EmptyProps, EmptySize, EmptyVariant } from './components/empty';
+export type { PopoverAlign, PopoverAnchorProps, PopoverCloseProps, PopoverPosition, PopoverProps, PopoverTriggerProps } from './components/popover';
 export type { ProgressProps, ProgressSize, ProgressVariant } from './components/progress';
 export type { RadioGroupProps, RadioItemProps, RadioOrientation } from './components/radio';
 export type { SelectOptionProps, SelectProps, SelectSize } from './components/select';
@@ -57,4 +135,5 @@ export type { SwitchProps, SwitchSize, SwitchVariant } from './components/switch
 export type { TableBodyProps, TableCaptionProps, TableCellProps, TableHeadProps, TableHeaderProps, TableProps, TableRowProps, TableSize } from './components/table';
 export type { TabGroupProps, TabListProps, TabPanelProps, TabTriggerProps } from './components/tab';
 export type { TextInputProps } from './components/text-input';
+export type { TextareaProps, TextareaResize, TextareaSize } from './components/textarea';
 export type { TooltipProps, TooltipPosition } from './components/tooltip';
